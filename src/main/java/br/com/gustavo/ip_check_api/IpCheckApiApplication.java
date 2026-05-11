1c76c98699a2fd6fb919eb8ae554fc49b23db192
Gustavo Wallace <Clasherbr@gmail.com>
2026-05-11 14:51:53 -0300

    base package structure

---
+++ src/main/java/br/com/gustavo/ip_check_api/IpCheckApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.ip_check_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpCheckApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpCheckApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gustavo/ip_check_api/IpCheckApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.ip_check_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpCheckApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
