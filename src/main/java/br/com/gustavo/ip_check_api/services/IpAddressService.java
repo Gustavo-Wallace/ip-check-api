c285b47b6685681ec9bb51be6071e73a486179dd
Gustavo Wallace <Clasherbr@gmail.com>
2026-05-20 13:12:18 -0300

    Add IP address sevice

---
+++ src/main/java/br/com/gustavo/ip_check_api/services/IpAddressService.java
@@ -0,0 +1,46 @@
+package br.com.gustavo.ip_check_api.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.com.gustavo.ip_check_api.dtos.IpAddressRequestDTO;
+import br.com.gustavo.ip_check_api.dtos.IpAddressResponseDTO;
+import br.com.gustavo.ip_check_api.models.IpAddress;
+import br.com.gustavo.ip_check_api.repositories.IpAddressRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class IpAddressService {
+
+    private final IpAddressRepository ipAddressRepository;
+
+    public IpAddressResponseDTO create(IpAddressRequestDTO requestDTO) {
+        IpAddress ipAddress = IpAddress.builder()
+                .address(requestDTO.getAddress())
+                .description(requestDTO.getDescription())
+                .build();
+
+        IpAddress savedIpAddress = ipAddressRepository.save(ipAddress);
+
+        return toResponseDTO(savedIpAddress);
+    }
+
+    public List<IpAddressResponseDTO> findAll() {
+        return ipAddressRepository.findAll()
+                .stream()
+                .map(this::toResponseDTO)
+                .toList();
+    }
+
+    private IpAddressResponseDTO toResponseDTO(IpAddress ipAddress) {
+        return IpAddressResponseDTO.builder()
+                .id(ipAddress.getId())
+                .address(ipAddress.getAddress())
+                .description(ipAddress.getDescription())
+                .active(ipAddress.getActive())
+                .createdAt(ipAddress.getCreatedAt())
+                .build();
+    }
+}
